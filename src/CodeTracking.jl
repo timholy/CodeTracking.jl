e04b194b7f1ebe6a8c8b54f7359cab8740347259
Lyndon White <oxinabox@ucc.asn.au>
2019-08-22 11:59:53 +0100

    Update src/CodeTracking.jl
    Co-Authored-By: Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>

--- src/CodeTracking.jl
+++ src/CodeTracking.jl
@@ -188,7 +188,7 @@ end
 Return a string with the code that defines `method`. Also return the first line of the
 definition, including the signature (which may not be the same line number returned
 by `whereis`).
-If the method could not be found for some reason, returns `nothing`.
+If the method could not be found for some reason, then return `nothing`.
 
 Note this may not be terribly useful for methods that are defined inside `@eval` statements;
 see [`definition(Expr, method::Method)`](@ref) instead.
