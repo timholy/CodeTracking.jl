7343b9669105fe9e89fd52fe6dbbbb15a97924c1
Zentrik <Zentrik@users.noreply.github.com>
2023-08-09 19:12:19 +0100

    Fix issue 115 (#117)

--- test/runtests.jl
+++ test/runtests.jl
@@ -251,6 +251,12 @@ isdefined(Main, :Revise) ? Main.Revise.includet("script.jl") : include("script.j
     CodeTracking.invoked_setindex!(d, sin, "sin")
     @test CodeTracking.invoked_get!(Vector{Function}, d, :cos) isa Vector{Function}
 
+    # Issue 115, Cthulhu issue 404
+    m = @which (Vector)(Int[])
+    src, line = definition(String, m)
+    @test occursin("(Array{T,N} where T)(x::AbstractArray{S,N}) where {S,N}", src)
+    @test line == m.line
+
     # Issue 115, Cthulhu issue 474
     m = @which NamedTuple{(),Tuple{}}(())
     src, line = definition(String, m)
